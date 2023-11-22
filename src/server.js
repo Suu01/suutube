4f9590ecdfb2ece8b96d436ea08f9c0fd9119395
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-11-22 20:49:06 +0900

    create server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
 
-app.listen(4000, () => console.log("Server Listening on port 4000"));
+app.listen(4000, () =>
+  console.log(`Server Listening on port http://localhost:${PORT}`)
+);
